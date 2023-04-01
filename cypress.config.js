74952bd65f32477cc4fd9e42d95aae275842a9b0
Shubham Pawar <shubham.pawar110997@gmail.com>
2023-04-01 11:26:51 -0700

    updated code

---
+++ cypress.config.js
@@ -0,0 +1,27 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  reporter: "cypress-mochawesome-reporter",
+  reporterOptions: {
+    charts: true,
+    embeddedScreenshots: false,
+    inlineAssets: true,
+    saveAllAttempts: false,
+  },
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+      require("cypress-mochawesome-reporter/plugin")(on);
+    },
+
+    baseUrl: "https://stage.clearpointstrategy.com/#/login",
+    chromeWebSecurity: false,
+    viewportWidth: 1366,
+    viewportHeight: 768,
+    retries: 1,
+    env: {
+      DEFAULT_USER_EMAIL: "cps_testrig@ascendantsmg.com",
+      DEFAULT_USER_PASSWORD: "abc123",
+    },
+  },
+});
